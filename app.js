9ea5c85aea4dc6fd2553d9c6a940a59d3c726899
Dan Johal <dan.johal2@gmail.com>
2021-02-16 12:37:55 -0800

    removed secondoperand getting closer to working function

--- app.js
+++ app.js
@@ -1,12 +1,85 @@
 //Query Selectors
 let display = document.querySelector(".display");
 let operatorButtons = document.querySelectorAll(".operator");
-let equalButton = document.querySelectorAll(".equal");
+let equalButton = document.querySelector(".equal");
 let numberButtons = document.querySelectorAll(".number");
-let clearButton = document.querySelectorAll(".clearentry");
+let clearButton = document.querySelector(".clearentry");
 let eraseButton = document.querySelector(".allclear");
 let decimalButton = document.querySelector(".decimal");
-display.textContent = "0";
+let posneg = document.querySelector(".posneg");
+
+
+
+let firstOperand = [];
+let currentOperation = [];
+let result = [];
+
+
+eraseButton.addEventListener('click', () => {
+    display.textContent = "0";
+    firstOperand = [];
+    currentOperation = [];
+    result = [];
+})
+
+numberButtons.forEach((button) => 
+    button.addEventListener('click', () => appendNumber(button.textContent))
+);
+
+operatorButtons.forEach((button) => 
+    button.addEventListener('click', () => setOperation(button.textContent))
+);
+
+function appendNumber(number) {
+    if (display.textContent === "0") {
+        display.textContent = "";
+        display.textContent = number;
+        
+    } else {
+        display.textContent += number;
+    }
+}
+
+function setOperation(operator) {
+    currentOperation = operator;
+    display.textContent = "";
+    
+console.log(firstOperand);
+console.log(currentOperation);
+}
+
+
+function evaluate(firstOperand, currentOperation, result) {
+    if (firstOperand && currentOperation) {
+        display.textContent = result;
+    }
+    a = firstOperand;
+    b = result;
+    switch (currentOperation) {
+        case "+":
+            result = add(a, b);
+            display.textContent = result;
+        case "-":
+            result = subtract(a, b);
+            display.textContent = result;
+
+        case "*":
+            result = multiply(a, b);
+            display.textContent = result;
+
+        case "/":
+             if (b === 0) return "INFINITY";
+             else result = divide(a, b);
+             display.textContent = result;
+
+        default:
+            return null;   
+}
+}
+
+
+equalButton.addEventListener('click', evaluate(firstOperand, currentOperation, result));
+
 
 //Functions for math operation function
 let add = (a, b) => {
@@ -44,4 +117,3 @@ let operate = (operator, a, b) => {
     }
 }
 
-console.log(operate("-", 4, 10));
